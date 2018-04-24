d3760501f6a5f3c6681d627ea4f9c50b9abd4460
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-04-24 09:41:13 +0300

    Small configs

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,17 @@
 import express from "express";
+import logger from "morgan";
+import helmet from "helmet";
 
 class App {
   constructor() {
     this.app = express();
+    this.config();
   }
   public app: express.Application;
+  public config = (): void => {
+    this.app.use(logger("dev"));
+    this.app.use(helmet());
+  };
 }
 
 export default new App().app;
