6bf50466f301fbf1e2778570ec020c23674dc54c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-04-24 14:43:37 +0300

    Holly shit. Got typeorm working and connected to Postgres

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import app from "./app";
+import { createConnection, Connection } from "typeorm";
 
 const PORT: number | string = process.env.PORT || 4000;
 
@@ -8,5 +9,10 @@ const handleAppError = (error: NodeJS.ErrnoException): void =>
 const handleListening = (): void =>
   console.log(`Listening on http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening);
+createConnection()
+  .then(connection => {
+    app.listen(PORT, handleListening);
+  })
+  .catch(error => console.log(error));
+
 app.on("error", handleAppError);
