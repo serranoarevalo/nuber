eb2bc07bd54bd76ef009dd9d498a0622deb1da6e
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-04-25 14:36:05 +0300

    Didn't really like Kenex

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,26 +1,17 @@
 import express from "express";
 import logger from "morgan";
 import helmet from "helmet";
-import Knex from "knex";
-import { Model } from "objection";
-import knexConfig from "../knexfile";
 
 class App {
   public app: express.Application;
 
   constructor() {
-    this.db();
     this.app = express();
     this.config();
   }
   private middlewares = (): void => {
     this.app.use(logger("dev"));
     this.app.use(helmet());
-  };
-  private db = (): void => {
-    const knex = Knex(knexConfig.development);
-    knex.migrate.latest();
-    Model.knex(knex);
   };
   private config = (): void => {
     this.middlewares();
