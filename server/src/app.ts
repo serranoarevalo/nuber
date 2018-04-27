b6ae10999460d5bf86b3bc22babc38045884a470
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-04-27 16:59:56 +0300

    Append token to the req object middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,11 @@
 import { GraphQLServer } from "graphql-yoga";
+import { NextFunction, Request, Response } from "express";
 import logger from "morgan";
 import helmet from "helmet";
+import jwt from "jsonwebtoken";
 import schema from "./schema";
 import entities from "./entities";
+import { JWT_SECRET } from "./keys";
 
 class App {
   public app: GraphQLServer;
@@ -20,8 +23,26 @@ class App {
     this.middlewares();
   }
   private middlewares = (): void => {
+    this.app.express.use(this.appendUserToReq);
     this.app.express.use(logger("dev"));
     this.app.express.use(helmet());
+  };
+
+  private appendUserToReq = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    const token = req.get("X-JWT");
+    if (token) {
+      try {
+        const decoded = await jwt.verify(token, JWT_SECRET);
+        req.user = decoded["user"];
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    next();
   };
 }
 
