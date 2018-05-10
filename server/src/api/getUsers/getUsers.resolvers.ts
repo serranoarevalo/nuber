dc122067f535b33cc5c10bd69979eb2084f6e1ef
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-05-10 16:37:33 +0300

    Added graphql to nodemon, getUsers query

--- server/src/api/getUsers/getUsers.resolvers.ts
+++ server/src/api/getUsers/getUsers.resolvers.ts
@@ -1,5 +1,22 @@
+import { authenticatedResolver } from "../../utils/wrappedResolvers";
+
 module.exports = {
   Query: {
-    getUser: () => "stuff"
+    getUsers: authenticatedResolver.wrap(
+      async (parent, args, { entities: { User } }) => {
+        const users = await User.find();
+        if (users) {
+          return {
+            ok: true,
+            users
+          };
+        } else {
+          return {
+            ok: false,
+            error: "Could not find all the users"
+          };
+        }
+      }
+    )
   }
 };
