bfb8f98a402eef236fa39c0601a8475927368529
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-05-11 08:54:01 +0300

    [Migrated] addPhone

---
+++ server/src/api/User/addPhone/addPhone.resolvers.ts
@@ -0,0 +1,30 @@
+import { makeMiddleware, authMiddleware } from "../../../utils/middlewares";
+import { sendVerificationText } from "../../../utils/sendSMS";
+import Confirmation from "../../../entities/Confirmation";
+import User from "../../../entities/User";
+import { Resolvers } from "../../../types/resolvers";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    addPhone: makeMiddleware(
+      authMiddleware,
+      async (_, { phoneNumber }: { phoneNumber: string }, { req }) => {
+        const { user }: { user: User } = req;
+        user.phoneNumber = phoneNumber;
+        user.save();
+        const confirmation: Confirmation = await Confirmation.create({
+          user,
+          type: "phone"
+        }).save();
+        const message = await sendVerificationText(
+          phoneNumber,
+          confirmation.key
+        );
+        return {
+          ok: true
+        };
+      }
+    )
+  }
+};
+export default resolvers;
