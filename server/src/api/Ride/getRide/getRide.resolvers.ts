45e2fd9505af2dbe99a25cd4c7fe2b1f3b5d094a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-05-16 19:22:14 +0300

    Types

--- server/src/api/Ride/getRide/getRide.resolvers.ts
+++ server/src/api/Ride/getRide/getRide.resolvers.ts
@@ -3,6 +3,7 @@ import { makeMiddleware, authMiddleware } from "../../../utils/middlewares";
 import Ride from "../../../entities/Ride";
 import User from "../../../entities/User";
 import { getConnection } from "typeorm";
+import { GetRideResponse } from "../../../types/graph";
 
 interface IArgs {
   rideId: number;
@@ -10,35 +11,38 @@ interface IArgs {
 
 const resolvers: Resolvers = {
   Query: {
-    getRide: makeMiddleware(authMiddleware, async (_, args: IArgs, { req }) => {
-      const { user }: { user: User } = req;
-      const ride = await getConnection()
-        .getRepository(Ride)
-        .createQueryBuilder("ride")
-        .loadAllRelationIds()
-        .where(
-          "ride.id = :rideId AND ride.passenger = :userId OR ride.driver = :driverId",
-          {
-            userId: user.id,
-            driverId: user.id,
-            rideId: args.rideId
-          }
-        )
-        .getOne();
-      if (ride) {
-        return {
-          ok: true,
-          ride,
-          error: null
-        };
-      } else {
-        return {
-          ok: false,
-          ride: null,
-          error: "Couldn't find ride"
-        };
+    getRide: makeMiddleware(
+      authMiddleware,
+      async (_, args: IArgs, { req }): Promise<GetRideResponse> => {
+        const { user }: { user: User } = req;
+        const ride = await getConnection()
+          .getRepository(Ride)
+          .createQueryBuilder("ride")
+          .loadAllRelationIds()
+          .where(
+            "ride.id = :rideId AND ride.passenger = :userId OR ride.driver = :driverId",
+            {
+              userId: user.id,
+              driverId: user.id,
+              rideId: args.rideId
+            }
+          )
+          .getOne();
+        if (ride) {
+          return {
+            ok: true,
+            ride,
+            error: null
+          };
+        } else {
+          return {
+            ok: false,
+            ride: null,
+            error: "Couldn't find ride"
+          };
+        }
       }
-    })
+    )
   }
 };
 
--- server/src/api/Ride/getRideHistory/getRideHistory.resolvers.ts
+++ server/src/api/Ride/getRideHistory/getRideHistory.resolvers.ts
@@ -1,36 +1,39 @@
 import { Resolvers } from "../../../types/resolvers";
 import { makeMiddleware, authMiddleware } from "../../../utils/middlewares";
-import { User } from "../../../types/graph";
+import { User, GetRideHistoryResponse } from "../../../types/graph";
 import Ride from "../../../entities/Ride";
 import { getConnection } from "typeorm";
 
 const resolvers: Resolvers = {
   Query: {
-    getRideHistory: makeMiddleware(authMiddleware, async (_, __, { req }) => {
-      const { user }: { user: User } = req;
-      const rides = await getConnection()
-        .getRepository(Ride)
-        .createQueryBuilder("ride")
-        .loadAllRelationIds()
-        .where("ride.passenger = :userId OR ride.driver = :driverId", {
-          userId: user.id,
-          driverId: user.id
-        })
-        .getMany();
-      if (rides) {
-        return {
-          ok: true,
-          rides,
-          error: null
-        };
-      } else {
-        return {
-          ok: false,
-          rides: null,
-          error: "Could not find rides"
-        };
+    getRideHistory: makeMiddleware(
+      authMiddleware,
+      async (_, __, { req }): Promise<GetRideHistoryResponse> => {
+        const { user }: { user: User } = req;
+        const rides = await getConnection()
+          .getRepository(Ride)
+          .createQueryBuilder("ride")
+          .loadAllRelationIds()
+          .where("ride.passenger = :userId OR ride.driver = :driverId", {
+            userId: user.id,
+            driverId: user.id
+          })
+          .getMany();
+        if (rides) {
+          return {
+            ok: true,
+            rides,
+            error: null
+          };
+        } else {
+          return {
+            ok: false,
+            rides: null,
+            error: "Could not find rides"
+          };
+        }
       }
-    })
+    )
   }
 };
 export default resolvers;
