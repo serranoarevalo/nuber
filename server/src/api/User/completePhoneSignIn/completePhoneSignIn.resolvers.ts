7231ecc5dbd81fac567e5925b2130a4862562189
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-05-17 17:56:37 +0300

    completePhoneSignIn

---
+++ server/src/api/User/completePhoneSignIn/completePhoneSignIn.resolvers.ts
@@ -0,0 +1,42 @@
+import { Resolvers } from "../../../types/resolvers";
+import Confirmation from "../../../entities/Confirmation";
+import { CompletePhoneSignInResponse } from "../../../types/graph";
+import User from "../../../entities/User";
+import { createJWT } from "../../../utils/createJWT";
+
+interface IArgs {
+  key: string;
+}
+
+const resolvers: Resolvers = {
+  Mutation: {
+    completePhoneSignIn: async (
+      _,
+      { key }: IArgs
+    ): Promise<CompletePhoneSignInResponse> => {
+      const confirmation: Confirmation = await Confirmation.findOne(
+        {
+          key
+        },
+        { relations: ["user"] }
+      );
+      if (confirmation) {
+        const user: User = confirmation.user;
+        const token: string = createJWT(user.id);
+        await confirmation.remove();
+        return {
+          ok: true,
+          token,
+          error: null
+        };
+      } else {
+        return {
+          ok: false,
+          token: null,
+          error: "Login token is not valid or has expired."
+        };
+      }
+    }
+  }
+};
+export default resolvers;
