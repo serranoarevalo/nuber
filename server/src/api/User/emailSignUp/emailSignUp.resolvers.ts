09214d68453aadb76e32cdb98384e79e91ed4b19
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-05-17 18:00:58 +0300

    Checking for pre existing users in email signup

--- server/src/api/User/emailSignUp/emailSignUp.resolvers.ts
+++ server/src/api/User/emailSignUp/emailSignUp.resolvers.ts
@@ -15,6 +15,14 @@ interface IArgs {
 const resolvers: Resolvers = {
   Mutation: {
     emailSignUp: async (_, args: IArgs, __): Promise<EmailSignUpResponse> => {
+      const existingUser: User = await User.findOne({ email: args.email });
+      if (existingUser) {
+        return {
+          ok: false,
+          user: null,
+          error: "User already exists, try to sign in."
+        };
+      }
       const newUser: User = await User.create(args).save();
       const emailConfirmation: Confirmation = await Confirmation.create({
         user: newUser
