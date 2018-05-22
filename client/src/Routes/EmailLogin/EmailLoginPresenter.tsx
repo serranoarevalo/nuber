20fe80d65e21bb5bfa5e8bb75df964f93e97bb7c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-05-22 21:53:10 +0300

    Enter/Leave transitions working

--- client/src/Routes/EmailLogin/EmailLoginPresenter.tsx
+++ client/src/Routes/EmailLogin/EmailLoginPresenter.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 
+const Spring = require("react-spring");
+
 interface IProps {
   email: string;
   handleInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const EmailLoginPresenter: React.SFC<IProps> = ({ email }) => (
-  <div>
-    <span>hello</span>
-  </div>
+  <Spring.Transition
+    from={{ color: "red" }}
+    enter={{ color: "yellow" }}
+    leave={{ color: "black" }}
+  >
+    {styles => (
+      <div style={styles}>
+        <span>hello</span>
+      </div>
+    )}
+  </Spring.Transition>
 );
 
 export default EmailLoginPresenter;
