1f4346d25283adf241ae3f67706f47713101b288
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-05-28 23:33:07 +0300

    OMG FINALLY YAAAAS

--- client/src/Components/Header/HeaderPresenter.tsx
+++ client/src/Components/Header/HeaderPresenter.tsx
@@ -6,27 +6,20 @@ import styled from "styled-components";
 const Container = styled<any, any>("div")`
   background-color: black;
   width: 100%;
-  height: ${props => {
-    if (props.scrollHeight < 8) {
-      return `${13 - props.scrollHeight}vh;`;
-    } else {
-      return "5vh;";
-    }
-  }};
+  height: ${props => (props.scrollHeight < 3 ? "13vh;" : "5vh;")};
   color: white;
   padding: 15px;
   display: flex;
-  flex-direction: ${props => (props.scrollHeight < 10 ? "column" : "row")};
-  justify-content: ${props =>
-    props.scrollHeight < 5 ? "space-between" : "flex-start"};
+  flex-direction: column;
+  justify-content: space-between;
   position: fixed;
   width: 100%;
   transition: height 0.1s linear;
   box-shadow: ${props =>
-      props.scrollHeight > 5
-        ? "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);"
-        : "none;"}
-    span {
+    props.scrollHeight > 5
+      ? "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);"
+      : "none;"};
+  span {
     color: white;
     font-size: 18px;
   }
@@ -34,12 +27,15 @@ const Container = styled<any, any>("div")`
 
 const Title = styled<any, any>("h2")`
   font-size: 30px;
+  transform-origin: 0% 0%;
   transition: transform 0.1s linear;
-  transform-origin: 0%;
-  transform: ${props =>
-    props.scrollHeight < 15
-      ? `translateX(${props.scrollHeight * 2.5}px)  scale(.7)`
-      : `translateX(35px)  scale(.7)`};
+  transform: ${props => {
+    if (props.scrollHeight > 3) {
+      return `translate(50px, -20px) scale(.7);`;
+    } else {
+      return `none;`;
+    }
+  }};
 `;
 
 interface IProps {
