8065d7d4baf619e747b60369765d37b4a3c7a9ce
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-06-01 21:18:39 +0300

    Menu Links

--- client/src/Routes/Home/Menu/MenuPresenter.tsx
+++ client/src/Routes/Home/Menu/MenuPresenter.tsx
@@ -1,8 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  height: 100%;
+`;
 
-const MenuPresenter: React.SFC = () => <Container>lalaala</Container>;
+const Header = styled.div`
+  background-color: black;
+  height: 20%;
+  margin-bottom: 30px;
+`;
+
+const SLink = styled(Link)`
+  font-size: 26px;
+  display: block;
+  margin-left: 15px;
+  margin-bottom: 25px;
+  font-weight: 400;
+`;
+
+const MenuPresenter: React.SFC = () => (
+  <Container>
+    <Header />
+    <SLink to="/">Payment</SLink>
+    <SLink to="/">Your Trips</SLink>
+    <SLink to="/">Settings</SLink>
+  </Container>
+);
 
 export default MenuPresenter;
