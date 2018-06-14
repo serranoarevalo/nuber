3f8489e0bb13909943b491283409f8430898ab38
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-06-14 11:31:49 +0300

    Ejected from Apollo Boost

--- client/src/apollo.ts
+++ client/src/apollo.ts
@@ -1,51 +1,81 @@
-import ApolloClient, { Operation } from "apollo-boost";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { ApolloLink } from "apollo-link";
+import { setContext } from "apollo-link-context";
+import { onError } from "apollo-link-error";
+import { HttpLink } from "apollo-link-http";
+import { withClientState } from "apollo-link-state";
+import { toast } from "react-toastify";
 
-const client = new ApolloClient({
-  clientState: {
-    defaults: {
-      auth: {
-        __typename: "Auth",
-        isLoggedIn: Boolean(localStorage.getItem("jwt")) || false
-      }
-    },
-    resolvers: {
-      Mutation: {
-        logUserOut: (_, __, { cache }) => {
-          localStorage.removeItem("jwt");
-          cache.writeData({
-            data: {
-              auth: {
-                __typename: "Auth",
-                isLoggedIn: false
-              }
+const cache = new InMemoryCache();
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("jwt");
+  return {
+    headers: {
+      ...headers,
+      "X-JWT": token || ""
+    }
+  };
+});
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/graphql"
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) => {
+      toast.error(`Unexpected error: ${message}`);
+    });
+  }
+  if (networkError) {
+    toast.error(`Network error: ${networkError}`);
+  }
+});
+
+const localStateLink = withClientState({
+  defaults: {
+    auth: {
+      __typename: "Auth",
+      isLoggedIn: Boolean(localStorage.getItem("jwt")) || false
+    }
+  },
+  resolvers: {
+    Mutation: {
+      logUserOut: (_, __, { cache: localCache }) => {
+        localStorage.removeItem("jwt");
+        localCache.writeData({
+          data: {
+            auth: {
+              __typename: "Auth",
+              isLoggedIn: false
             }
-          });
-          location.reload();
-          return null;
-        },
-        logUserIn: (_, { token }, { cache }) => {
-          localStorage.setItem("jwt", token);
-          cache.writeData({
-            data: {
-              auth: {
-                __typename: "Auth",
-                isLoggedIn: true
-              }
+          }
+        });
+        location.reload();
+        return null;
+      },
+      logUserIn: (_, { token }, { cache: localCache }) => {
+        localStorage.setItem("jwt", token);
+        localCache.writeData({
+          data: {
+            auth: {
+              __typename: "Auth",
+              isLoggedIn: true
             }
-          });
-          return null;
-        }
+          }
+        });
+        return null;
       }
     }
   },
-  request: async (operation: Operation) => {
-    operation.setContext({
-      headers: {
-        "X-JWT": localStorage.getItem("jwt") || ""
-      }
-    });
-  },
-  uri: "http://localhost:4000/graphql"
+  cache
+});
+
+const client = new ApolloClient({
+  link: ApolloLink.from([errorLink, localStateLink, authLink.concat(httpLink)]),
+  cache
 });
 
 export default client;
