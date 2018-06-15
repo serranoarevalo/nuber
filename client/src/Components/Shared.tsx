154747c2f5b53b3659fe431988f0f33717efa755
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-06-15 19:15:58 +0300

    BS

--- client/src/Components/Shared.tsx
+++ client/src/Components/Shared.tsx
@@ -2,8 +2,12 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
+  padding: 0px 15px;
   padding-top: 150px;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 export const Wrapper = styled.div.attrs({
   className: "shouldScroll"
